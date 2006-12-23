5ef921d8e6918b5094441942d76786f7dd682549
Jonas Genannt <jonas@brachium-system.net>
2006-12-23 23:53:25 +0000

    Reverted: Rev. 18. Wrong Dump.

--- mail_system.sql
+++ mail_system.sql
@@ -1,55 +1,64 @@
--- MySQL dump 10.10
---
--- Host: localhost    Database: mail_system
--- ------------------------------------------------------
--- Server version	5.0.30-Debian_1-log
+-- 
+-- Host: 127.0.0.1
+-- Generation Time: Dec 24, 2006 at 12:52 AM
+-- Server version: 4.1.11
+-- PHP Version: 4.3.10-16
+-- 
+-- Database: `mail_system`
+-- 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- --------------------------------------------------------
 
---
+-- 
 -- Table structure for table `adm_users`
---
+-- 
 
 DROP TABLE IF EXISTS `adm_users`;
-CREATE TABLE `adm_users` (
+CREATE TABLE IF NOT EXISTS `adm_users` (
   `id` int(11) NOT NULL auto_increment,
   `username` varchar(200) collate utf8_unicode_ci NOT NULL default '',
   `passwd` varchar(200) collate utf8_unicode_ci NOT NULL default '',
   `access` enum('y','n') collate utf8_unicode_ci NOT NULL default 'y',
   `manager` enum('n','y') collate utf8_unicode_ci NOT NULL default 'n',
   `full_name` varchar(255) collate utf8_unicode_ci default NULL,
-  `cpasswd` varchar(255) character set utf8 NOT NULL,
+  `cpasswd` varchar(255) character set utf8 NOT NULL default '',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
+-- 
+-- Dumping data for table `adm_users`
+-- 
+
+INSERT INTO `adm_users` VALUES (1, 'admin', '', 'y', 'y', 'Superadmin', '$1$Ekjbn5PV$lTKL1k2IkDKzpneppf6Wx0');
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `admin_access`
---
+-- 
 
 DROP TABLE IF EXISTS `admin_access`;
-CREATE TABLE `admin_access` (
+CREATE TABLE IF NOT EXISTS `admin_access` (
   `id` int(11) NOT NULL auto_increment,
   `email` int(11) NOT NULL default '0',
   `domain` int(11) NOT NULL default '0',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
+-- 
+-- Dumping data for table `admin_access`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `autoresponder`
---
+-- 
 
 DROP TABLE IF EXISTS `autoresponder`;
-CREATE TABLE `autoresponder` (
+CREATE TABLE IF NOT EXISTS `autoresponder` (
   `id` int(11) NOT NULL auto_increment,
   `esubject` varchar(100) collate utf8_unicode_ci NOT NULL default '',
   `msg` text collate utf8_unicode_ci NOT NULL,
@@ -59,12 +68,19 @@ CREATE TABLE `autoresponder` (
   KEY `email` (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
+-- 
+-- Dumping data for table `autoresponder`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `autoresponder_send`
---
+-- 
 
 DROP TABLE IF EXISTS `autoresponder_send`;
-CREATE TABLE `autoresponder_send` (
+CREATE TABLE IF NOT EXISTS `autoresponder_send` (
   `id` int(11) NOT NULL auto_increment,
   `email` int(11) NOT NULL default '0',
   `efromto` varchar(255) collate utf8_unicode_ci NOT NULL default '',
@@ -73,12 +89,19 @@ CREATE TABLE `autoresponder_send` (
   KEY `in2` (`email`,`efromto`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
+-- 
+-- Dumping data for table `autoresponder_send`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `domains`
---
+-- 
 
 DROP TABLE IF EXISTS `domains`;
-CREATE TABLE `domains` (
+CREATE TABLE IF NOT EXISTS `domains` (
   `id` int(11) NOT NULL auto_increment,
   `dnsname` varchar(255) collate utf8_unicode_ci NOT NULL default '',
   `access` enum('y','n') collate utf8_unicode_ci NOT NULL default 'y',
@@ -92,27 +115,41 @@ CREATE TABLE `domains` (
   UNIQUE KEY `dnsname_2` (`dnsname`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
+-- 
+-- Dumping data for table `domains`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `email_options`
---
+-- 
 
 DROP TABLE IF EXISTS `email_options`;
-CREATE TABLE `email_options` (
+CREATE TABLE IF NOT EXISTS `email_options` (
   `id` int(11) NOT NULL auto_increment,
-  `email` int(11) NOT NULL,
-  `conf` varchar(100) collate utf8_unicode_ci default NULL,
-  `options` varchar(100) collate utf8_unicode_ci default NULL,
-  `extra` varchar(100) collate utf8_unicode_ci default NULL,
+  `email` int(11) NOT NULL default '0',
+  `conf` varchar(100) default NULL,
+  `options` varchar(100) default NULL,
+  `extra` varchar(100) default NULL,
   PRIMARY KEY  (`id`),
   KEY `conf` (`conf`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
+-- 
+-- Dumping data for table `email_options`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `forwardings`
---
+-- 
 
 DROP TABLE IF EXISTS `forwardings`;
-CREATE TABLE `forwardings` (
+CREATE TABLE IF NOT EXISTS `forwardings` (
   `id` int(11) NOT NULL auto_increment,
   `domainid` int(11) NOT NULL default '0',
   `efrom` varchar(255) collate utf8_unicode_ci NOT NULL default '',
@@ -122,46 +159,59 @@ CREATE TABLE `forwardings` (
   UNIQUE KEY `efrom` (`efrom`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
+-- 
+-- Dumping data for table `forwardings`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `list_recp`
---
+-- 
 
 DROP TABLE IF EXISTS `list_recp`;
-CREATE TABLE `list_recp` (
+CREATE TABLE IF NOT EXISTS `list_recp` (
   `id` int(11) NOT NULL auto_increment,
   `recp` varchar(100) default NULL,
   KEY `listID` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
+-- 
+-- Dumping data for table `list_recp`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `lists`
---
+-- 
 
 DROP TABLE IF EXISTS `lists`;
-CREATE TABLE `lists` (
+CREATE TABLE IF NOT EXISTS `lists` (
   `id` int(11) NOT NULL auto_increment,
-  `domainid` int(11) NOT NULL,
-  `address` varchar(80) NOT NULL,
+  `domainid` int(11) NOT NULL default '0',
+  `address` varchar(80) NOT NULL default '',
   `access` enum('y','n') NOT NULL default 'y',
-  `public` enum('y','n') NOT NULL,
+  `public` enum('y','n') NOT NULL default 'y',
   PRIMARY KEY  (`id`),
   KEY `second` (`address`,`access`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-/*!50003 SET @OLD_SQL_MODE=@@SQL_MODE*/;
-DELIMITER ;;
-/*!50003 SET SESSION SQL_MODE="" */;;
-/*!50003 CREATE */ /*!50017 DEFINER=`root`@`localhost` */ /*!50003 TRIGGER `listDelete` AFTER DELETE ON `lists` FOR EACH ROW begin delete from list_recp where listID = OLD.listID; end */;;
+-- 
+-- Dumping data for table `lists`
+-- 
 
-DELIMITER ;
-/*!50003 SET SESSION SQL_MODE=@OLD_SQL_MODE */;
 
---
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `mailfilter`
---
+-- 
 
 DROP TABLE IF EXISTS `mailfilter`;
-CREATE TABLE `mailfilter` (
+CREATE TABLE IF NOT EXISTS `mailfilter` (
   `id` int(11) NOT NULL auto_increment,
   `email` int(11) NOT NULL default '0',
   `active` int(11) NOT NULL default '1',
@@ -172,12 +222,19 @@ CREATE TABLE `mailfilter` (
   KEY `email` (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
+-- 
+-- Dumping data for table `mailfilter`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `spamassassin`
---
+-- 
 
 DROP TABLE IF EXISTS `spamassassin`;
-CREATE TABLE `spamassassin` (
+CREATE TABLE IF NOT EXISTS `spamassassin` (
   `username` varchar(100) NOT NULL default '',
   `preference` varchar(30) NOT NULL default '',
   `value` varchar(100) NOT NULL default '',
@@ -187,12 +244,19 @@ CREATE TABLE `spamassassin` (
   KEY `preference` (`preference`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
+-- 
+-- Dumping data for table `spamassassin`
+-- 
+
+
+-- --------------------------------------------------------
+
+-- 
 -- Table structure for table `users`
---
+-- 
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL auto_increment,
   `domainid` int(11) NOT NULL default '0',
   `email` varchar(255) collate utf8_unicode_ci NOT NULL default '',
@@ -206,18 +270,12 @@ CREATE TABLE `users` (
   `disablewebmail` tinyint(1) default '1',
   `spamassassin` tinyint(1) NOT NULL default '0',
   `move_spam` varchar(100) collate utf8_unicode_ci default NULL,
-  `cpasswd` varchar(255) character set utf8 NOT NULL,
+  `cpasswd` varchar(255) character set utf8 NOT NULL default '',
   PRIMARY KEY  (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- 
+-- Dumping data for table `users`
+-- 
 
--- Dump completed on 2006-12-21 14:58:08
