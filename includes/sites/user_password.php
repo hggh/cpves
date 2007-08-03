d04d12202ad109796f09d13d2be568e01e8bf517
Jonas Genannt <jonas@brachium-system.net>
2007-08-03 20:59:04 +0000

    user_password.tpl/php

---
+++ includes/sites/user_password.php
@@ -0,0 +1,65 @@
+<?php
+/******************************************************************************
+* Copyright (C) 2006 Jonas Genannt <jonas.genannt@brachium-system.net>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+******************************************************************************/
+
+$smarty->assign('email', $_SESSION['email']);
+if (isset($_POST['u_submit']))
+{
+	if (!isset($_POST['new_passwd1']) || empty($_POST['new_passwd1']) ||
+	    !isset($_POST['new_passwd2']) || empty($_POST['new_passwd2']))
+	{
+		$smarty->assign('error_msg','y');
+		$smarty->assign('if_error_password_empty','y');
+	}
+	else if (check_passwd_length($_POST['new_passwd2']) ==false)
+	{
+		$smarty->assign('error_msg','y');
+		$smarty->assign('if_error_password_long', 'y');
+		}
+	else if($_POST['new_passwd1'] !=$_POST['new_passwd2'] )
+	{
+		$smarty->assign('error_msg','y');
+		$smarty->assign('if_new_passwd_not_same', 'y');
+	}
+	else if(decrypt_passwd($_SESSION['cpasswd']) != $_POST['old_passwd'])
+	{
+		$smarty->assign('error_msg','y');
+		$smarty->assign('if_error_password_old_wrong','y');
+	}
+	else
+	{
+		if ($config['cleartext_passwd']==1) {
+			$cleartext=$_POST['new_passwd1'];
+		}
+		else
+		{
+			$cleartext="";
+		}
+		$sql=sprintf("UPDATE users SET passwd='%s',cpasswd='%s' WHERE id='%d'",
+			$db->escapeSimple($cleartext),
+			$db->escapeSimple(crypt($_POST['new_passwd1'])),
+			$db->escapeSimple($_SESSION['uid']));
+		$res=&$db->query($sql);
+		
+		$smarty->assign('passwd_changed', 'y');
+		$smarty->assign('success_msg','y');
+		$smarty->assign('if_password_changed', 'y');
+		$_SESSION['cpasswd']=encrypt_passwd($_POST['new_passwd1']);
+	}
+}
+?>
---
+++ templates/user_password.tpl
@@ -0,0 +1,25 @@
+<br/>
+<form action="?module=user_password" method="post">
+<table>
+<tr>
+ <td>E-Mailadresse:</td>
+ <td>{$email}</td>
+</tr>
+<tr>
+ <td>Altes Passwort:</td>
+ <td><input type="password" name="old_passwd" /></td>
+</tr>
+<tr>
+ <td>Neues Passwort:</td>
+ <td><input name="new_passwd1" maxlength="{$max_passwd_len}" type="password" /></td>
+</tr>
+<tr>
+ <td>Wiederholen:</td>
+ <td><input name="new_passwd2" maxlength="{$max_passwd_len}" type="password" /></td>
+</tr>
+<tr>
+ <td colspan="2" style="text-align:center;">
+ <input name="u_submit" value="&Auml;ndern" type="submit" /></td>
+</tr>
+</table>
+</form>
