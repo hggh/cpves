900d4c93045d4510787e36690cea958ee381ba6d
Stefan Koop <mail@stefan-koop.de>
2013-08-23 12:31:55 +0200

    removed rails directory

--- rails/db/migrate/20091203221722_add_domain_email_reference_to_email_types.rb
+++
@@ -1,9 +0,0 @@
-class AddDomainEmailReferenceToEmailTypes < ActiveRecord::Migration
-  def self.up
-    add_column :domain_emails, :domain_email_type_id, :integer
-  end
-
-  def self.down
-    remove_column :domain_emails, :domain_email_type_id
-  end
-end
